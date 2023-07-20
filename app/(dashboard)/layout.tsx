5aa12a078207966889fccdf5a8e0ee11876bb9bb
Tushar98644 <b.toshar@iitg.ac.in>
2023-07-20 12:43:49 +0530

    project structure

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,9 @@
+const Dashboard_Layout = ({children}:{children:React.ReactNode}) => {
+    return ( 
+        <div>
+            {children}
+        </div>
+     );
+}
+ 
+export default Dashboard_Layout;
